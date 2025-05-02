3ee4072dcc0af88e7063e929a9036d5301f2d014
Yuval Adar <adar.yuval@gmail.com>
2025-05-02 22:17:46 +0300

    installer v0.1 alpha

--- bootstrap/bootstrap-arch.sh
+++ bootstrap/bootstrap-arch.sh
@@ -1,5 +1,5 @@
 #little env prep
-export PATH="$PATH:/$HOME/.local/bin"
+export PATH="$PATH:$HOME/.local/bin"
 
 # bare essentials
 sudo pacman -Sy --noconfirm curl git less base-devel wget zsh python3 tmux htop btop vim nvim tree fzf jq zip unzip
@@ -10,6 +10,7 @@ if [ ! -d ~/.nocture_tmp ]; then
 fi
 
 if [ -d ~/.nocturne_tmp/yay ]; then rm -rf ~/.nocturne_tmp/yay; fi
+
 git clone https://aur.archlinux.org/yay-bin.git ~/.nocturne_tmp/yay
 cd ~/.nocturne_tmp/yay
 makepkg -si --noconfirm
@@ -19,7 +20,7 @@ cd -
 rm -rf ~/.nocturne_tmp/yay
 
 # install all shell tools
-sudo pacman -Sy --noconfirm fastfetch github-cli lazygit zellij fzf ripgrep bat eza zoxide plocate btop tldr fd
+sudo pacman -Sy --noconfirm fastfetch github-cli lazygit zellij fzf ripgrep bat eza zoxide plocate btop tldr fd starship atuin thefuck
 
 sudo pacman -Sy --noconfirm autoconf bison clang rust openssl readline libyaml readline zlib ncurses libffi gdbm jemalloc imagemagick mupdf mupdf-tools sqlite mise gnupg gum
 

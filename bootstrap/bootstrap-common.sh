9cbe532f3e99969634d7a6114564db612b8f3609
Yuval Adar <adar.yuval@gmail.com>
2025-05-09 17:26:34 +0300

    installer v0.1 alpha

--- bootstrap/bootstrap-common.sh
+++ bootstrap/bootstrap-common.sh
@@ -11,7 +11,7 @@ if [[ -d ~/.oh-my-zsh ]]; then
     fmt_info "oh-my-zsh already installed."
 else
     fmt_info "Installing oh-my-zsh and setting zsh as default shell."
-    gum spin --title="Installing oh-my-zsh..." -- sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended 2>&1 | tee -a $NOCTURNE_LOG/bootstrap.log
+    gum spin --show-output --title="Installing oh-my-zsh..." -- sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended >>$NOCTURNE_LOG/bootstrap.log
     sudo -k chsh -s /usr/bin/zsh "$USER"
 fi
 

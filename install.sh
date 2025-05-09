741ee1d2a82214c08cd23d4fd39e4179df5441ca
Yuval Adar <adar.yuval@gmail.com>
2025-05-09 18:29:50 +0300

    installer v0.1 alpha

--- install.sh
+++ install.sh
@@ -265,8 +265,7 @@ print_header
 if [ -d $NOCTURNE_GIT ]; then
     fmt_warning "Old version of Nocturne detected at $NOCTURNE_GIT"
     fmt_warning "Proceeding with installation will delete this folder!"
-    yn=$(gum confirm "Do you want to delete $NOCTURNE_GIT and download the latest version?")
-    echo $yn
+    gum confirm "Do you want to delete $NOCTURNE_GIT and download the latest version?" && yn=0 || yn=1
     case $yn in
     0)
         fmt_warning "Deleting $NOCTURNE_GIT"
@@ -274,7 +273,7 @@ if [ -d $NOCTURNE_GIT ]; then
         break
         ;;
     1)
-        exit 0
+        exit
         ;;
     esac
 fi
@@ -284,7 +283,7 @@ git clone https://github.com/adaryorg/nocturne-dev.git $NOCTURNE_GIT >/dev/null 
 # show the disclaimer!
 gum pager <$NOCTURNE_GIT/disclaimer
 
-yn=$(gum confirm "Do you want to proceed with Nocturne installation?")
+gum confirm "Do you want to proceed with Nocturne installation?" && yn=0 || yn=1
 case $yn in
 0) break ;;
 1) exit ;;
